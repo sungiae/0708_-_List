852854af3ff6d6accf2ee9f8e44bdef26a7823c8
박성재 <@210.178.175.58>
2021-07-08 18:49:51 +0900

    11

---
+++ 20210708-컬렉션(List)/src/com/sist/client/client.java
@@ -0,0 +1,12 @@
+package com.sist.client;
+import java.util.*;
+import java.net.*;
+import java.io.*;
+public class client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
